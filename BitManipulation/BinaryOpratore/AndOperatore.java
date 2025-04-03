f04f377d4115f7dd9a9a120d48f7c6a17d5e45ad
joshankumar6299 <joshankumar6299@gmail.com>
2025-04-03 13:36:03 +0530

    Add implementation of bitwise AND operation in AndOperatore.java

---
+++ BitManipulation/BinaryOpratore/AndOperatore.java
@@ -0,0 +1,15 @@
+package BitManipulation.BinaryOpratore;
+
+public class AndOperatore {
+    public static void main(String[] args) {
+        int a=6;
+        int b=5;
+        int c=a&b;
+
+        // Binary number of 6 is 110 and 5 is 101
+        // 110 & 101 = 100
+
+        
+        System.out.println(c);
+    }
+}
