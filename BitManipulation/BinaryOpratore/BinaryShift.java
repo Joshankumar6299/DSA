ff0ad21378f98b4d64e2586814b07dd3cb0a7fc5
joshankumar6299 <joshankumar6299@gmail.com>
2025-04-03 13:36:13 +0530

    Add implementation of binary shift operations in BinaryShift.java

---
+++ BitManipulation/BinaryOpratore/BinaryShift.java
@@ -0,0 +1,15 @@
+package BitManipulation.BinaryOpratore;
+
+public class BinaryShift {
+    public static void main(String[] args) {
+        
+
+        // Binary Left shift
+
+        System.out.println(5<<2);
+
+        // Binary Rigth shift
+
+        System.out.println(5>>2);
+    }
+}
