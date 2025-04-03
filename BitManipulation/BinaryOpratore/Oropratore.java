205897c7dcf7068282cadb3fdb3b74511e014daf
joshankumar6299 <joshankumar6299@gmail.com>
2025-04-03 13:36:25 +0530

    Add implementation of bitwise OR operation in Oropratore.java

---
+++ BitManipulation/BinaryOpratore/Oropratore.java
@@ -0,0 +1,13 @@
+package BitManipulation.BinaryOpratore;
+
+public class Oropratore {
+    public static void main(String[] args) {
+        int a=6;
+        int b=5;
+        int c=a|b;
+
+        // Binary number of 6 is 110 and 5 is 101
+        // 110 | 101 = 111
+        System.out.println(c);
+    }
+}
